eeadd9ab900d1129b03af92f72ce9c0357b41892
Giancarlo Angela <giancarloangela@Giancarlos-MacBook-Pro.local>
2019-06-03 00:14:05 +0200

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,9 +19,20 @@ const IndexPage = () => (
     </Section>
     <Section>
       <Col span={12}>
-        <form name="dolo" netlify>
-          <input type="text" value="smoel" />
-          <button>awo</button>
+        <form name="contact" netlify>
+          <p>
+            <label>
+              Name <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Email <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
         </form>
       </Col>
       <Col span={12}>intro</Col>
