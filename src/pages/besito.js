26f30ac70ff2c0c71081ea461e6424e76cd9081b
giancarlo <giancarlo@messagebird.com>
2020-01-11 23:07:40 +0100

    add animation

--- src/pages/besito.js
+++ src/pages/besito.js
@@ -1,11 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { useCountdownTimer } from "../hooks/useCountdownTimer"
 import BBDushi from "../static/images/tutu.jpg"
 import Paps from "../static/images/paps.png"
 import "../static/css/hehe.css"
+import styled from "styled-components"
+
+const Besito = styled.div`
+  position: absolute;
+  bottom: 32px;
+  font-size: 70px;
+`
 
 const BesitoTimer = () => {
+  const [showBesito, setShowBesito] = useState(false)
+
   const { days, hours, minutes, seconds } = useCountdownTimer("2020-02-13")
+
+  useEffect(() => {
+    setShowBesito(showBesito => !showBesito)
+  }, [seconds])
+
   return (
     <div className="main-wrapper" style={{ height: "100%" }}>
       <div className="besito-wrapper">
@@ -19,6 +33,7 @@ const BesitoTimer = () => {
           <span className="time">
             {`${hours} hours ${minutes} minutes ${seconds} seconds`}
           </span>
+          {showBesito && <Besito>😘</Besito>}
         </div>
       </div>
     </div>
