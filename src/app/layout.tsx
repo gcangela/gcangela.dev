07629a7f1e48fce1716b91cc96bdf2e66b800570
gcangela <gcangela90@gmail.com>
2023-09-13 12:13:36 +0200

    add gtag script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from "next/script";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <Script
+        id={'gtag'}
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+    />
+
+    <Script strategy="lazyOnload" id={'gtag-datalayer'}>
+      {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+    </Script>
       <body className={inter.className}>{children}</body>
     </html>
   )
