01d3f7353e08aab2edb208a09bf1cc08beb513ee
giancarlo <gcangela90@gmail.com>
2025-04-10 15:58:02 +0200

    smoel

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,21 +1,32 @@
+import { Tab, Tabs } from "@/components/Tabs";
+import { tabContent } from "./content";
 
-import { Tab, Tabs } from "@/components/Tabs"
-import { tabContent } from "./content"
+const aboutPageTabs = ["about", "experience", "education", "skills"] as const;
+export type PageTabs = (typeof aboutPageTabs)[number];
 
-
-const aboutPageTabs = ["about", "experience", "education", "skills"] as const
-export type PageTabs = typeof aboutPageTabs[number]
-
-export default async function AboutPage({ searchParams }: { searchParams: Promise<{ tab?: PageTabs }> }) {
-    const params = await searchParams
-    const currentTab = params.tab && aboutPageTabs.includes(params.tab) ? params.tab : "about"
-    return <div className="flex flex-col md:flex-row relative top-0 h-full">
-        <div className="flex flex-1">
-            <Tabs currentTab={currentTab || 'about'}>
-                {aboutPageTabs.map((tab) => <Tab key={`about-page-tab: ${tab}`} title={tabContent[tab].title} value={tab}>
-                    {tabContent[tab].content}
-                </Tab>)}
-            </Tabs >
-        </div>
+export default async function AboutPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ tab?: PageTabs }>;
+}) {
+  const params = await searchParams;
+  const currentTab =
+    params.tab && aboutPageTabs.includes(params.tab) ? params.tab : "about";
+  return (
+    <div className="flex flex-col md:flex-row relative top-0 h-full">
+      <div className="flex flex-1">
+        <Tabs currentTab={currentTab || "about"}>
+          {aboutPageTabs.map((tab) => (
+            <Tab
+              key={`about-page-tab: ${tab}`}
+              title={tabContent[tab].title}
+              value={tab}
+            >
+              {tabContent[tab].content}
+            </Tab>
+          ))}
+        </Tabs>
+      </div>
     </div>
+  );
 }
